b4ef6d3cc8bd18da9fa9339c4ff8eb9535c73b09
Babatunde Ololade <babatundeololade50@gmail.com>
2022-10-20 17:02:58 +0300

    📦 NEW: add welcome message

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-const clearConsole = require('clear-any-console');
+welcome({
+  title: pkgJSON.name,
+  tagLine: `Get to know Babatunde`,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#0077ff`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+});
 
-clearConsole();
-
-console.log('Babatunde Ololade');
+console.log(`Babatunde Ololade`);
